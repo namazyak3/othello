1e36869b8cac38e4cf1bdb276d47fd348234d638
namazyak3 <namazyake@gmail.com>
2023-05-17 21:49:56 +0900

    [update] #2 置いた座標と行・列の石リストを取得できるようにした。

--- client/src/components/main/viewer/table/index.tsx
+++ client/src/components/main/viewer/table/index.tsx
@@ -1,60 +1,72 @@
-import { NextPage } from "next"
-import styles from "./table.module.scss"
-import _tableData from "./init.json"
-import { useState } from "react"
-import Stone from "./stone"
-import { lstat } from "fs"
+import { NextPage } from "next";
+import styles from "./table.module.scss";
+import _tableData from "./init.json";
+import { useState } from "react";
+import Stone from "./stone";
 
 const Table: NextPage = () => {
-  // false => 1, true => 2
-  const [turn, setTurn] = useState<1|2>(1)
-  const toggleTurn = () => setTurn(turn == 1 ? 2 : 1)
-  const [tableData, setTableData] = useState<number[][]>(_tableData)
+  //* -1: 黒, 1: 白
+  const [turn, setTurn] = useState<1 | -1>(1);
+  const toggleTurn = () => setTurn(turn == 1 ? -1 : 1);
+  const [tableData, setTableData] = useState<number[][]>(_tableData);
 
   const setStone = (row: number, column: number) => {
-    tableData[row][column] = turn
-    toggleTurn()
+    //tableData[row][column] = turn;
+    toggleTurn();
 
-    const getList = (type: "row"|"col"|"dia") => {
-      if (type=="row") {
-        return tableData[row]
-      }
-      else if (type=="col") {
-        let ls: number[] = []
-        for (let i = 0; i < 3; i++) {
-          ls.push(tableData[i][column])
-        }
-        return ls
-      }
-      else {
-        if (row == 0) {
-          if (column == 0) {
-            
-          }
-        } else if (row == 2) {
-          return [tableData[0][2], tableData[1][1], tableData[2][0]]
-        }
-      }
-    }
-    console.log(getList("row"))
-    console.log(getList("col"))
-  }
- 
+    console.log(
+      `置いた: {行列: ${row},列: ${column}, ターン: ${turn == 1 ? "白" : "黒"}}`
+    );
+
+    // row
+    console.log("行", tableData[row]);
+
+    // column
+    console.log(
+      "列",
+      Object.keys(tableData).map((key, i) => tableData[i][column])
+    );
+
+    // diaglam
+    //console.log(`上: ${row}, 右: ${7 - column}, 下: ${7 - row}, 左: ${column}`);
+    const r_u = Math.min(7 - column, row);
+    const r_d = Math.min(7 - column, 7 - row);
+    const l_d = Math.min(column, 7 - row);
+    const l_u = Math.min(column, row);
+
+    // diaglam(ld > ru)
+    const ld_ru = [...Array(r_u + l_d + 1)].map((key, ii) => {
+      return 1;
+    });
+    console.log("左下 > 右上", ld_ru);
+
+    // diaglam(rr > ld)
+    const ru_ld = [...Array(r_d + l_u + 1)].map((key, ii) => {
+      return 1;
+    });
+    console.log("右上 > 左下", ru_ld);
+  };
+
   return (
     <>
-    <table className={styles.table}>
-      <tbody>
-        {Object.keys(tableData).map((key, i) => (
-          <tr key={key}>
-            {Object.keys(tableData[i]).map((key, ii) => (
-              <td key={key}><Stone side={tableData[i][ii]} handler={() => setStone(i, ii)} /></td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
+      <table className={styles.table}>
+        <tbody>
+          {Object.keys(tableData).map((key, i) => (
+            <tr key={key}>
+              {Object.keys(tableData[i]).map((key, ii) => (
+                <td key={key}>
+                  <Stone
+                    side={tableData[i][ii]}
+                    handler={() => setStone(i, ii)}
+                  />
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
-  )
-}
+  );
+};
 
-export default Table
+export default Table;
